465274f990efb1669ec060727e2e625427283811
Jatin <digrajatin@gmail.com>
2025-08-16 01:49:28 +0530

    begin

---
+++ Aug25/342-Power_of_4.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+int main(){
+	// check if a number is power of 4 or not
+	// Approach 1 -> recursive/loop based, keep diving by 4, remainder should be 0
+	
+	return 0;
+}
---
+++ CppStudy/inputPlayGround.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(){
+
+	std::string line;
+	std::cin>>line;
+	std::cout<<line;
+	std::cout<<std::endl;
+	std::string line2;
+	std::cin.ignore();
+	std::getline(std::cin, line2);
+	std::cout<<line2;
+
+	return 0;
+}
